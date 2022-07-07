8b7f4920fdbd1d2a3cdf0ff33e4ce2c5d4fd220d
achensetyawati <setyawati@moonlay.com>
2022-07-07 16:40:50 +0700

    sales-return upload

---
+++ Com.Kana.Service.Upload.Lib/Migrations/20220707094022_Update_Sales_return.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Com.Kana.Service.Upload.Lib.Migrations
+{
+    public partial class Update_Sales_return : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "typeAutoNumber",
+                table: "AccuSalesReturns",
+                newName: "TypeAutoNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "toAddress",
+                table: "AccuSalesReturns",
+                newName: "ToAddress");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TypeAutoNumber",
+                table: "AccuSalesReturns",
+                newName: "typeAutoNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "ToAddress",
+                table: "AccuSalesReturns",
+                newName: "toAddress");
+        }
+    }
+}
---
+++ Com.Kana.Service.Upload.Lib/ViewModels/AccuSalesReturnViewModel/AccuSalesReturnDetailSerialNumberViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Com.Kana.Service.Upload.Lib.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Kana.Service.Upload.Lib.ViewModels.AccuSalesReturnViewModel
+{
+    public class AccuSalesReturnDetailSerialNumberViewModel : BaseViewModel
+    {
+        public string status { get; set; }
+        public DateTimeOffset expiredDate { get; set; }
+        public double quantity { get; set; }
+        public string serialNumberNo { get; set; }
+        public virtual long accuSalesInvoceDetailItemId { get; set; }
+        public virtual AccuSalesReturnDetailItemViewModel accuSalesReturnDetailItem { get; set; }
+    }
+}
