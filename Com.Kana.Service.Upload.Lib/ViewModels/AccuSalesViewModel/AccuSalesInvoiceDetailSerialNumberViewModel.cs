b293eec20ae7cd0e72acd380f131778221198063
Lucia Widyana <mo_cia@yahoo.com>
2022-07-08 14:53:53 +0700

    update viewMdel

--- Com.Kana.Service.Upload.Lib/ViewModels/AccuSalesViewModel/AccuSalesInvoiceDetailSerialNumberViewModel.cs
+++ Com.Kana.Service.Upload.Lib/ViewModels/AccuSalesViewModel/AccuSalesInvoiceDetailSerialNumberViewModel.cs
@@ -11,7 +11,7 @@ namespace Com.Kana.Service.Upload.Lib.ViewModels.AccuSalesViewModel
 		public DateTimeOffset expiredDate { get; set; }
 		public double quantity { get; set; }
 		public string serialNumberNo { get; set; }
-		public virtual long accuSalesInvoceDetailItemId { get; set; }
-		public virtual AccuSalesInvoiceDetailItemViewModel accuSalesInvoiceDetailItem { get; set; }
+		public long accuSalesInvoceDetailItemId { get; set; }
+	 
 	}
 }
