bb19790ff4e8ac43695abc656b9054caf4027b93
Lucia Widyana <mo_cia@yahoo.com>
2022-07-22 10:45:30 +0700

    embuhh meneh

---
+++ Com.Kana.Service.Upload.Lib/ViewModels/AccuSalesViewModel/SalesSearchResultViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Com.Kana.Service.Upload.Lib.ViewModels.AccuSalesViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Kana.Service.Upload.Lib.ViewModels.AccuSalesViewModel
+{
+	public class SalesSearchResultViewModel
+	{
+		public bool s { get; set; }
+		public List<AccuSalesUploadViewModel> d { get; set; }
+		public Page sp { get; set; }
+	}
+
+	public class Page
+	{
+		public long page { get; set; }
+		public long pageCount { get; set; }
+		public long rowCount { get; set; }
+	}
+}
--- Com.Kana.Service.Upload.Lib/ViewModels/ItemSearchResultViewModel.cs
+++ Com.Kana.Service.Upload.Lib/ViewModels/ItemSearchResultViewModel.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Com.Kana.Service.Upload.Lib.ViewModels 
+namespace Com.Kana.Service.Upload.Lib.ViewModels
 {
     public class ItemSearchResultViewModel
     {
